a559e49ed4d788204b051e81e4ad2e65e8307336
Serhii Pemakhov <serhii.pemakhov@onix-systems.com>
2024-04-05 17:27:25 +0300

    feat: add HTTP constants

---
+++ src/constants/http-method.constants.ts
@@ -0,0 +1,23 @@
+export const CONNECT = 'CONNECT';
+export const DELETE = 'DELETE';
+export const GET = 'GET';
+export const HEAD = 'HEAD';
+export const OPTIONS = 'OPTIONS';
+export const PATCH = 'PATCH';
+export const POST = 'POST';
+export const PUT = 'PUT';
+export const TRACE = 'TRACE';
+
+const HTTP_METHODS = {
+  CONNECT,
+  DELETE,
+  GET,
+  HEAD,
+  OPTIONS,
+  PATCH,
+  POST,
+  PUT,
+  TRACE,
+};
+
+export default HTTP_METHODS;
