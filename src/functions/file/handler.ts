aed019f65c40c16f2edd1fdc2f97cb06271463d2
Serhii Pemakhov <serhii.pemakhov@onix-systems.com>
2024-04-10 11:08:36 +0300

    docs: update JS doc

--- src/functions/file/handler.ts
+++ src/functions/file/handler.ts
@@ -4,9 +4,9 @@ import { formatJSONResponse } from '@libs/api-gateway';
 import { APIGatewayProxyEvent, APIGatewayProxyHandler } from 'aws-lambda';
 
 /**
- * @function: ./src/functions/file/handler.getAll
- * @description: Get all files
- * @returns: { files: UploadedFileType[] }
+ * @function: src/functions/file/handler.getAll
+ * @description: Gets all files
+ * @returns: { files: File[] }
  * @example: curl -X GET http://localhost:3000/dev/files/
  */
 const getAll = middify(async () => {
@@ -17,6 +17,24 @@ const getAll = middify(async () => {
   });
 });
 
+/**
+ * @function: src/functions/file/handler.getSignedUploadUrl
+ * @description: Gets a file upload presigned URL
+ * @returns: {
+ *   "url": "https://{bucket-name}.s3.{region}.amazon.com/",
+ *   "fields": {
+ *     "x-amz-storage-class": "INTELLIGENT_TIERING",
+ *     "bucket": "{bucket-name}",
+ *     "X-Amz-Algorithm": "AWS4-HMAC-SHA256",
+ *     "X-Amz-Credential": "{AWS_ACCESS_KEY_ID}/{current-date}/{region}/s3/aws4_request",
+ *     "X-Amz-Date": "20240410T074048Z",
+ *     "key": "182a3a48-362a-4605-a74b-b95a86b3a6bc_file.pdf",
+ *     "Policy": "eyJ...XX0=",
+ *     "X-Amz-Signature": "203...982"
+ *   }
+ * }
+ * @example: curl -X POST http://localhost:3000/dev/files/signed-upload-url?fileType=application%2Fpdf&fileName=file.pdf
+ */
 const getSignedUploadUrl: APIGatewayProxyHandler = middify(
   {
     type: 'object',
