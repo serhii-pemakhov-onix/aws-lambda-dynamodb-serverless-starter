5431fdb552fe5ff5121c73dc0ab839cedcf90ca1
Serhii Pemakhov <serhii.pemakhov@onix-systems.com>
2024-04-13 18:39:08 +0300

    refactor: rename constants

--- src/constants/dynamodb.constants.ts
+++ src/constants/dynamodb.constants.ts
@@ -1,5 +1,5 @@
 export const TABLE_NAMES = {
-  USERS: process.env.USERS_TABLE || 'UsersTable',
-  SHOPS: process.env.SHOPS_TABLE || 'ShopsTable',
-  FILES: process.env.FILES_TABLE || 'FilesTable',
-};
+  USERS: 'UsersTable',
+  SHOPS: 'ShopsTable',
+  FILES: 'FilesTable2',
+} as const;
